76188758dda209700659342865794707ccac0383
hyejin <hyejin.marichal@le-campus-numerique.fr>
2021-02-22 11:42:05 +0100

    install leaflet

--- src/components/CitiesMap.vue
+++ src/components/CitiesMap.vue
@@ -1,43 +1,91 @@
+<!--<template>-->
+<!--  <div>-->
+<!--    <h1>Carte des villes</h1>-->
+<!--    <div id="map"></div>-->
+<!--  </div>-->
+<!--</template>-->
 <template>
-  <div>
-    <h1>Carte des villes</h1>
-    <div id="map"></div>
+  <div style="height: 100vh; width: 100vw;">
+    <l-map
+        v-model="zoom"
+        v-model:zoom="zoom"
+        :center="[45.8910906, 6.0925617]"
+    >
+      <l-tile-layer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      ></l-tile-layer>
+
+      <l-marker v-for="city in $store.state.cities" :lat-lng="[city.lat, city.lon]" :key="city.name">
+        <l-icon :icon-url="`https://openweathermap.org/img/wn/${city.icon}.png`" :icon-size="iconSize" />
+      </l-marker>
+    </l-map>
   </div>
 </template>
 
-<script lang="ts">
-import mapboxgl from "mapbox-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
+<!--<script lang="ts">-->
+<script>
+
+
 import {defineComponent} from 'vue';
-// eslint-disable-next-line no-unused-vars
-import City from "@/store/city";
+import {
+  LMap,
+  LTileLayer,
+  LMarker,
+  LIcon,
+} from "@vue-leaflet/vue-leaflet";
+
+import "leaflet/dist/leaflet.css";
 
 
 export default defineComponent({
   name: 'CitiesMap',
-  mounted() {
-    mapboxgl.accessToken =
-        "pk.eyJ1IjoiaGptYXJpY2hhbCIsImEiOiJja2w5ODd0MnQwNzYzMm5vb3g3NmY2ZGt0In0.UI6GQiO0BtxHM6VncDE3BQ";
-    const map = new mapboxgl.Map({
-      container: "map",
-      style: "https://maps.hotentic.com/styles/isere/style.json",
-      center: [process.env.VUE_APP_DEFAULT_LONGITUDE, process.env.VUE_APP_DEFAULT_LATITUDE],
-      zoom: 10,
-    });
-    map.on('load', async () => {
-      const mapboxgl = require('mapbox-gl/dist/mapbox-gl');
-      await this.$store.dispatch('getCities')
-      this.$store.state.cities.forEach((city: City) => {
-        let el = document.createElement('img');
-        el.src = `https://openweathermap.org/img/wn/${city.icon}@2x.png`;
-        el.classList.add('marker');
-        el.title = `${city.name} - ${city.temperature}°C`;
-        new mapboxgl.Marker(el)
-            .setLngLat([city.lon, city.lat])
-            .addTo(map);
-      });
-    });
+  data() {
+    const iconWidth = 50;
+    const iconHeight = 50;
+    return {
+      zoom:12 ,
+      iconWidth,
+      iconHeight,
+    }
   },
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LIcon,
+  },
+  computed: {
+    iconSize() {
+      return [this.iconWidth, this.iconHeight];
+    },
+  },
+
+  mounted() {
+    this.$store.dispatch('getCities')
+  }
+  // mounted() {
+  //   mapboxgl.accessToken =
+  //       "pk.eyJ1IjoiaGptYXJpY2hhbCIsImEiOiJja2w5ODd0MnQwNzYzMm5vb3g3NmY2ZGt0In0.UI6GQiO0BtxHM6VncDE3BQ";
+  //   const map = new mapboxgl.Map({
+  //     container: "map",
+  //     style: "https://maps.hotentic.com/styles/isere/style.json",
+  //     center: [process.env.VUE_APP_DEFAULT_LONGITUDE, process.env.VUE_APP_DEFAULT_LATITUDE],
+  //     zoom: 10,
+  //   });
+  //   map.on('load', async () => {
+  //     const mapboxgl = require('mapbox-gl/dist/mapbox-gl');
+  //     await this.$store.dispatch('getCities')
+  //     this.$store.state.cities.forEach((city: City) => {
+  //       let el = document.createElement('img');
+  //       el.src = `https://openweathermap.org/img/wn/${city.icon}@2x.png`;
+  //       el.classList.add('marker');
+  //       el.title = `${city.name} - ${city.temperature}°C`;
+  //       new mapboxgl.Marker(el)
+  //           .setLngLat([city.lon, city.lat])
+  //           .addTo(map);
+  //     });
+  //   });
+  // },
 });
 </script>
 
---
+++ src/components/CitiesMapMapbox.vue
@@ -0,0 +1,50 @@
+<!--<template>-->
+<!--  <div>-->
+<!--    <h1>Carte des villes</h1>-->
+<!--    <div id="map"></div>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<script lang="ts">-->
+<!--import mapboxgl from "mapbox-gl";-->
+<!--import "mapbox-gl/dist/mapbox-gl.css";-->
+<!--import {defineComponent} from 'vue';-->
+<!--// eslint-disable-next-line no-unused-vars-->
+<!--import City from "@/store/city";-->
+
+
+<!--export default defineComponent({-->
+<!--  name: 'CitiesMapbox',-->
+<!--  mounted() {-->
+<!--    mapboxgl.accessToken =-->
+<!--        "pk.eyJ1IjoiaGptYXJpY2hhbCIsImEiOiJja2w5ODd0MnQwNzYzMm5vb3g3NmY2ZGt0In0.UI6GQiO0BtxHM6VncDE3BQ";-->
+<!--    const map = new mapboxgl.Map({-->
+<!--      container: "map",-->
+<!--      style: "https://maps.hotentic.com/styles/isere/style.json",-->
+<!--      center: [process.env.VUE_APP_DEFAULT_LONGITUDE, process.env.VUE_APP_DEFAULT_LATITUDE],-->
+<!--      zoom: 10,-->
+<!--    });-->
+<!--    map.on('load', async () => {-->
+<!--      const mapboxgl = require('mapbox-gl/dist/mapbox-gl');-->
+<!--      await this.$store.dispatch('getCities')-->
+<!--      this.$store.state.cities.forEach((city: City) => {-->
+<!--        let el = document.createElement('img');-->
+<!--        el.src = `https://openweathermap.org/img/wn/${city.icon}@2x.png`;-->
+<!--        el.classList.add('marker');-->
+<!--        el.title = `${city.name} - ${city.temperature}°C`;-->
+<!--        new mapboxgl.Marker(el)-->
+<!--            .setLngLat([city.lon, city.lat])-->
+<!--            .addTo(map);-->
+<!--      });-->
+<!--    });-->
+<!--  },-->
+<!--});-->
+<!--</script>-->
+
+
+<!--<style scoped>-->
+<!--h1 {-->
+<!--  margin: 40px 0 0;-->
+<!--}-->
+
+<!--</style>-->
